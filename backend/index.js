32e5ba9fcafab1057915840444b8645e13c57d4f
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-08 22:24:58 +0200

    Connet to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const connectionString = process.env.DB_CONNECTION;
+
+mongoose
+  .connect(connectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 // const getVodLink = require("./getVodLink");
 // console.log(getVodLink(123123121123123, 60 * 60 * 24 + 123));
