35afa94cd5c523433951a7ec05d429ae1579d95b
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-08 16:38:05 +0200

    Add VodInfo model

---
+++ backend/models/VodInfo.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const VodInfoSchema = Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  stream_id: {
+    type: String,
+  },
+  user_id: {
+    type: String,
+  },
+  user_login: {
+    type: String,
+  },
+  user_name: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  created_at: {
+    type: String,
+  },
+  published_at: {
+    type: String,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  thumbnail_url: {
+    type: String,
+  },
+  viewable: {
+    type: String,
+    required: true,
+  },
+  view_count: {
+    type: Number,
+  },
+  language: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  muted_segments: [
+    {
+      duration: { type: Number },
+      offset: { type: Number },
+    },
+  ],
+});
+
+module.exports = mongoose.model("VodInfo", VodInfoSchema);
