0b02d6647120a16b475b60bd1d4055869ae7ae4f
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-18 21:58:53 +0200

    Refactor small functions

--- chat-downloader/services/TwitchService/downloadChatPiece.js
+++ chat-downloader/services/TwitchService/downloadChatPiece.js
@@ -3,50 +3,56 @@ const ChatMessage = require("../../models/ChatMessage");
 
 const SECONDS_IN_A_DAY = 24 * 3600;
 
-const shouldRemoveFirstComment = (comments, startSeconds) => {
-  const [firstComment] = comments;
-  return firstComment.content_offset_seconds < startSeconds;
+const isNotLastPage = (page, endSeconds) => {
+  const lastComment = page.comments[page.comments.length - 1];
+  return lastComment.content_offset_seconds < endSeconds && page._next;
 };
 
-const shouldRemoveLastComment = (comments, endSeconds) => {
-  const lastComment = comments[comments.length - 1];
-  return lastComment.content_offset_seconds >= endSeconds;
-};
-
-const isNotLastPage = (page, endSeconds) =>
-  !shouldRemoveLastComment(page.comments, endSeconds);
-
-const saveComments = async (comments) => {
+const remove_ids = (comments) => {
   comments.forEach((comment) => {
     delete comment._id;
     delete comment.commenter._id;
   });
+};
 
+const saveComments = async (comments) => {
   await ChatMessage.insertMany(comments);
 };
 
+const getNotEarlyComments = (comments, startSeconds) => {
+  const isNotEarly = ({ content_offset_seconds }) =>
+    content_offset_seconds >= startSeconds;
+
+  return comments.filter(isNotEarly);
+};
+
+const getNotLateComments = (comments, endSeconds) => {
+  const isNotLate = ({ content_offset_seconds }) =>
+    content_offset_seconds < endSeconds;
+
+  return comments.filter(isNotLate);
+};
+
 module.exports = async (vodId, startSeconds, endSeconds) => {
-  if (endSeconds > SECONDS_IN_A_DAY) {
+  const isTooLong = endSeconds > SECONDS_IN_A_DAY;
+
+  if (isTooLong) {
     throw RangeError("chat piece exceeds 24 hours");
   }
 
-  let currentPage = await twitchApi.getVodChatAtSeconds(vodId, startSeconds);
+  let chatPage = await twitchApi.getVodChatAtSeconds(vodId, startSeconds);
 
-  while (shouldRemoveFirstComment(currentPage.comments, startSeconds)) {
-    currentPage.comments.shift();
+  chatPage.comments = getNotEarlyComments(chatPage.comments, startSeconds);
+
+  while (isNotLastPage(chatPage, endSeconds)) {
+    remove_ids(chatPage.comments);
+    saveComments(chatPage.comments);
+
+    chatPage = await twitchApi.getVodChatPageAtCursor(vodId, chatPage._next);
   }
 
-  while (isNotLastPage(currentPage, endSeconds)) {
-    const { _next, comments } = currentPage;
+  chatPage.comments = getNotLateComments(chatPage.comments, endSeconds);
 
-    saveComments(comments);
-
-    currentPage = await twitchApi.getVodChatPageAtCursor(vodId, _next);
-  }
-
-  while (shouldRemoveLastComment(currentPage.comments, endSeconds)) {
-    currentPage.comments.pop();
-  }
-
-  await saveComments(currentPage.comments);
+  remove_ids(chatPage.comments);
+  await saveComments(chatPage.comments);
 };
