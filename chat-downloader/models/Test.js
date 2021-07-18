92fdafb41257199b5482b4f7c8a63e5ddf915242
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-18 10:18:32 +0200

    Create schemas with the new keyword

--- chat-downloader/models/Test.js
+++ chat-downloader/models/Test.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-
 const { Schema } = mongoose;
 
-const TestSchema = Schema({
+const TestSchema = new Schema({
   comment: {
     type: String,
   },
--- chat-downloader/models/VodInfo.js
+++ chat-downloader/models/VodInfo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-const VodInfoSchema = Schema({
+const VodInfoSchema = new Schema({
   id: {
     type: String,
     required: true,
