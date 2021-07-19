35facaa62b94884567228dd792b5ec7a0ca713e0
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-19 09:49:08 +0200

    Refactor to be more flexible

--- chat-downloader/services/TwitchService/getSecondsFromDuration.js
+++ chat-downloader/services/TwitchService/getSecondsFromDuration.js
@@ -1,16 +1,27 @@
+const SECONDS_IN_A_SECOND = 1;
 const SECONDS_IN_A_MINUTE = 60;
 const SECONDS_IN_AN_HOUR = 3600;
 
-const DURATION_REGEX = /^(\d+)h(\d+)m(\d+)s$/;
+const HOURS_POSTFIX = "h";
+const MINUTES_POSTFIX = "m";
+const SECONDS_POSTFIX = "s";
+
+const postfixToSecondsMapings = [
+  { postfix: HOURS_POSTFIX, seconds: SECONDS_IN_AN_HOUR },
+  { postfix: MINUTES_POSTFIX, seconds: SECONDS_IN_A_MINUTE },
+  { postfix: SECONDS_POSTFIX, seconds: SECONDS_IN_A_SECOND },
+];
+
+const getDigitsWithPostfixRegex = (postfix) => new RegExp(`(\\d+)${postfix}`);
 
 module.exports = (duration) => {
-  const match = duration.match(DURATION_REGEX);
+  const SECONDS_INIT = 0;
 
-  if (!match) {
-    throw TypeError("Invalid duration");
-  }
+  return postfixToSecondsMapings.reduce((seconds, mapping) => {
+    const regex = getDigitsWithPostfixRegex(mapping.postfix);
 
-  const [, hours, minutes, seconds] = match;
+    const match = duration.match(regex);
 
-  return hours * SECONDS_IN_AN_HOUR + minutes * SECONDS_IN_A_MINUTE + seconds;
+    return match ? seconds + match[1] : seconds;
+  }, SECONDS_INIT);
 };
