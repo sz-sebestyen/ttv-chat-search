0a9bb442ee73cd55210c9c04694da12f871bcc16
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-21 11:40:03 +0200

    Delete test model

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,3 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`app is listening at http://localhost:${port}`);
 });
-
-const Test = require("./models/Test");
-
-new Test({ comment: "can't touch this" }).save();
