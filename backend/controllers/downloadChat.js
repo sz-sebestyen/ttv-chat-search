ae75ff6369e4b29927beb37e670f9c7dc7ef318b
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-23 23:55:03 +0200

    Get chatDownloaderHost from env variable

--- backend/controllers/downloadChat.js
+++ backend/controllers/downloadChat.js
@@ -4,14 +4,19 @@ const asyncHandler = require("express-async-handler");
 const SUCCESS = 200;
 const NOT_FOUND = 404;
 
+const { CHAT_DOWNLOADER_HOST } = process.env;
+
 const downloadChat = async (req, res, next) => {
   const { id } = req.params;
 
   // TODO: check user is authenticated, if so, then save the vod request
 
-  const chatDownloaderResponse = await fetch(`http://localhost:5000/${id}`, {
-    method: "POST",
-  });
+  const chatDownloaderResponse = await fetch(
+    `${CHAT_DOWNLOADER_HOST}/vod/${id}`,
+    {
+      method: "POST",
+    }
+  );
 
   if (chatDownloaderResponse.status === SUCCESS) {
     res.json({ message: "Vod request registered" });
