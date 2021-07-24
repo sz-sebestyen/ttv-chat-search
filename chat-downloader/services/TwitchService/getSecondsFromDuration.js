fce9c763898b6dea7046ca854b57527027e50cdb
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-24 11:07:43 +0200

    Fix and refactor

--- chat-downloader/services/TwitchService/getSecondsFromDuration.js
+++ chat-downloader/services/TwitchService/getSecondsFromDuration.js
@@ -2,27 +2,21 @@ const SECONDS_IN_A_SECOND = 1;
 const SECONDS_IN_A_MINUTE = 60;
 const SECONDS_IN_AN_HOUR = 3600;
 
-const HOURS_POSTFIX = "h";
-const MINUTES_POSTFIX = "m";
-const SECONDS_POSTFIX = "s";
-
-const postfixToSecondsMapings = [
-  { postfix: HOURS_POSTFIX, seconds: SECONDS_IN_AN_HOUR },
-  { postfix: MINUTES_POSTFIX, seconds: SECONDS_IN_A_MINUTE },
-  { postfix: SECONDS_POSTFIX, seconds: SECONDS_IN_A_SECOND },
+const postfixToSecondsMaps = [
+  { postfix: "h", seconds: SECONDS_IN_AN_HOUR },
+  { postfix: "m", seconds: SECONDS_IN_A_MINUTE },
+  { postfix: "s", seconds: SECONDS_IN_A_SECOND },
 ];
 
-const getDigitsWithPostfixRegex = (postfix) => new RegExp(`(\\d+)${postfix}`);
+const SECONDS_INIT = 0;
 
 const getSecondsFromDuration = (duration) => {
-  const SECONDS_INIT = 0;
-
-  return postfixToSecondsMapings.reduce((seconds, mapping) => {
-    const regex = getDigitsWithPostfixRegex(mapping.postfix);
+  return postfixToSecondsMaps.reduce((allSeconds, { postfix, seconds }) => {
+    const regex = new RegExp(`(\\d+)${postfix}`);
 
     const match = duration.match(regex);
 
-    return match ? seconds + match[1] : seconds;
+    return match ? allSeconds + match[1] * seconds : allSeconds;
   }, SECONDS_INIT);
 };
 
