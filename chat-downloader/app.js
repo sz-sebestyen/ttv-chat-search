16de810bf046c5cb17240bb1dcdbacb7619b2130
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-25 17:43:27 +0200

    Move error handling into a middleware

--- chat-downloader/app.js
+++ chat-downloader/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 
-const SERVER_ERROR = 500;
-
 // controllers
 const vodController = require("./controllers/vod");
 
 // middlewares
 const downloadChat = require("./middlewares/downloadChat");
 const getVodInfo = require("./middlewares/getVodInfo");
+const errorHandler = require("./middlewares/errorHandler");
 
 // app-wide middlewares
 app.use(express.json());
@@ -18,14 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.post("/vod/:id/chat", [getVodInfo, vodController, downloadChat]);
 app.get("/vod/:id", [getVodInfo, vodController]);
 
-app.use((err, req, res, next) => {
-  if (err) {
-    console.log("server error: ", err);
-
-    res.status(SERVER_ERROR).json({ message: "Internal server error" });
-  } else {
-    next();
-  }
-});
+app.use(errorHandler);
 
 module.exports = app;
