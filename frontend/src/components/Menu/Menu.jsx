9f962514d31491b969c5a1a3aaf47a8a2ba8ed88
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-31 00:07:42 +0200

    Add menu state

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { VscMenu } from "react-icons/vsc";
+import React, { useState } from "react";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 
 function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
+
   return (
-    <div>
-      <VscMenu />
+    <div onClick={toggleMenu}>
+      {isOpen ? <HiOutlineX /> : <HiOutlineMenu />}
     </div>
   );
 }
