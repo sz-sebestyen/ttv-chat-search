6dddcf847d880ae48ef6c577c0beffdf6538c846
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-01 16:34:51 +0200

    Get vod id from user input

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const vodIdCaptureRegex =
+  /^((https:\/\/www\.)?twitch\.tv\/videos\/)?(?<vodId>\d+$)/;
 
 function Home() {
+  const [input, setInput] = useState("");
+  const [vodId, setVodId] = useState();
+
+  const storeInput = (event) => {
+    const newInputValue = event.target.value.trim();
+    setInput(newInputValue);
+
+    const match = newInputValue.match(vodIdCaptureRegex);
+
+    if (match) {
+      setVodId(match.groups.vodId);
+    } else {
+      // TODO: warn user
+    }
+  };
+
+  const getVodInfo = (id) => {
+    // TODO: fetch vodinfo from backend
+  };
+
+  useEffect(() => {
+    vodId && getVodInfo(vodId);
+  }, [vodId]); // eslint-disable-line
+
   return (
     <div>
       <p>Copy paste the link or id of the VOD</p>
-      <input type="text" className="border" />
+      <input
+        type="text"
+        className="border"
+        value={input}
+        onChange={storeInput}
+      />
     </div>
   );
 }
