d3f67124078150bb3aac90af52b632dbce06f529
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-05 22:43:46 +0200

    Add style to vod preview

--- frontend/src/components/VodInfoPreview.jsx
+++ frontend/src/components/VodInfoPreview.jsx
@@ -10,11 +10,25 @@ function VodInfoPreview({ vodId }) {
       .replace("%{height}", "180");
 
   return (
-    <div className="border">
+    <div className="">
       {vodInfo && (
-        <div>
-          <img src={getThumbnailUrl()} alt="Vod thumbnail" />
-          <p className="truncate" title={vodInfo.title}>
+        <div className="bg-surface rounded max-w-sm">
+          <p className="text-center p-1">{vodInfo.user_name}</p>
+
+          <div
+            style={{ backgroundImage: `url(${getThumbnailUrl()})` }}
+            className="h-40 bg-center bg-no-repeat h-[180px] w-[320px] p-2 m-auto grid grid-cols-2"
+          >
+            <span className="bg-surface bg-opacity-75 p-1.5 text-xs place-self-start">
+              {vodInfo.duration}
+            </span>
+
+            <span className="bg-surface bg-opacity-75 p-1.5 text-xs place-self-end">
+              {new Date(vodInfo.created_at).toLocaleString()}
+            </span>
+          </div>
+
+          <p className="truncate px-3 py-1" title={vodInfo.title}>
             {vodInfo.title}
           </p>
         </div>
