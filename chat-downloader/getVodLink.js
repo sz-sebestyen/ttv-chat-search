55502154e15e281a9fb7e544b1cc754ab62294de
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-08 16:06:20 +0200

    Remove days from link generation

--- chat-downloader/getVodLink.js
+++ chat-downloader/getVodLink.js
@@ -1,15 +1,10 @@
 const minutesInAnHour = 60;
-const hoursInADay = 24;
 
 const secondsInAMinute = 60;
 const secondsInAnHour = secondsInAMinute * minutesInAnHour;
-const secondsInADay = secondsInAnHour * hoursInADay;
 
 const getVodLink = (vod_id, content_offset_seconds) => {
   let secondsLeft = Math.floor(content_offset_seconds);
-
-  const days = Math.floor(secondsLeft / secondsInADay);
-  secondsLeft -= days * secondsInADay;
 
   const hours = Math.floor(secondsLeft / secondsInAnHour);
   secondsLeft -= hours * secondsInAnHour;
