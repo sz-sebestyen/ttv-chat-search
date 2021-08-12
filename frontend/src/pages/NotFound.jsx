13f64faeaf9011222eac088f6f169088dea1acae
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-12 22:03:20 +0200

    Add style to 404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function NotFound() {
-  return <div>404 Page not found</div>;
+  return (
+    <div className="text-3xl text-center bg-background p-2">
+      404 Page not found
+    </div>
+  );
 }
 
 export default NotFound;
