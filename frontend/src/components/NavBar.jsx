2e590292154a8057cec0f7e0dbbe23f1527e3662
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-12 22:15:05 +0200

    Make title a link to home

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Menu from "./Menu";
+import { useHistory } from "react-router-dom";
 
 function NavBar() {
+  const history = useHistory();
+
   return (
     <div className="flex justify-between items-center bg-background py-1 px-3 mb-2">
-      <h1 className="text-violet-400 text-xl p-1">TTV Chat Search</h1>
+      <h1
+        onClick={() => history.push("/")}
+        className="text-violet-400 text-xl p-1 cursor-pointer"
+      >
+        TTV Chat Search
+      </h1>
 
       <Menu />
     </div>
