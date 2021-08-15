34313eb9097f3b3786a0842aa397fd03c8011c9d
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-15 23:04:48 +0200

    Add links to navlist

--- frontend/src/components/NavLinkList.jsx
+++ frontend/src/components/NavLinkList.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const backendHost = process.env.REACT_APP_BACKEND_HOST;
 
 function NavLinkList() {
   return (
-    <ul className=" md:flex">
-      <li>asd</li>
-      <li>asd</li>
-      <li>asd</li>
+    <ul className="md:flex md:flex-row md:gap-4 text-center bg-background">
+      <ListItem>
+        <Link to="/search-history">Search history</Link>
+      </ListItem>
+      <ListItem>
+        <a href={`${backendHost}/login`}>Sign in</a>
+      </ListItem>
+      <ListItem>
+        <Link to="/logout">Sign out</Link>
+      </ListItem>
     </ul>
   );
+}
+
+function ListItem({ children }) {
+  return <li className="p-2">{children}</li>;
 }
 
 export default NavLinkList;
