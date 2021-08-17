dafc32dec50608e1d8c9fd5812fa7bad98e93563
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-17 19:52:13 +0200

    Style search history a little bit

--- frontend/src/pages/SearchHistory.jsx
+++ frontend/src/pages/SearchHistory.jsx
@@ -17,8 +17,15 @@ function SearchHistory() {
   return (
     <div>
       {searches.map((search) => (
-        <div>
-          {search.vodId}: {search.term}
+        <div className="px-4 py-2 bg-surface my-1 flex">
+          <span>{search.vodId}</span>
+          <span className="truncate">: {search.term}</span>
+          <a
+            href={`/vod/${search.vodId}/chat-search-result/${search.term}`}
+            className="ml-auto"
+          >
+            View
+          </a>
         </div>
       ))}
     </div>
