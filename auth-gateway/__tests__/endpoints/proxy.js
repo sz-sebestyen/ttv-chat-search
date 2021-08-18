2f7834c43510d004a21f7a33575604cb590caa13
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-19 00:44:08 +0200

    Fix proxy tests

--- auth-gateway/__tests__/endpoints/proxy.js
+++ auth-gateway/__tests__/endpoints/proxy.js
@@ -6,44 +6,39 @@ const nock = require("nock");
 const jwt = require("jsonwebtoken");
 
 describe("/*", () => {
+  // given
+  const sub = "user id";
+
+  const token = jwt.sign({ sub }, process.env.JWT_SECRET);
+
+  const path = "/custom-path";
+
+  nock(process.env.BACKEND_HOST, {
+    reqheaders: {
+      "x-user_id": (headerValue) => headerValue === sub,
+    },
+  })
+    .get(path)
+    .reply(200, "x-user_id included");
+
+  nock(process.env.BACKEND_HOST, {
+    badheaders: ["x-user_id"],
+  })
+    .get(path)
+    .reply(200, "no x-user_id header");
+
   describe("when no authorized Bearer token is in the header", () => {
     it("should proxy the request without the x-user_id in the header", async () => {
-      // given
-      const path = "/custom-path";
-
-      nock(process.env.BACKEND_HOST)
-        .get(path)
-        .reply(200, {
-          reqheaders: {
-            "X-user_id": (headerValue) => !headerValue,
-          },
-        });
-
       // when
       const response = request.get(path);
 
       // then
-      await response.expect(200);
+      await response.expect(200, "no x-user_id header");
     });
   });
 
   describe("when authorized Bearer token is in the header", () => {
     it("should proxy the request with the x-user_id included in the header", async () => {
-      // given
-      const sub = "user id";
-
-      const token = jwt.sign({ sub }, process.env.JWT_SECRET);
-
-      const path = "/custom-path";
-
-      nock(process.env.BACKEND_HOST)
-        .get(path)
-        .reply(200, {
-          reqheaders: {
-            "X-user_id": (headerValue) => headerValue === sub,
-          },
-        });
-
       // when
       const response = request
         .get(path)
@@ -51,7 +46,7 @@ describe("/*", () => {
         .set("authorization", `Bearer ${token}`);
 
       // then
-      await response.expect(200);
+      await response.expect(200, "x-user_id included");
     });
   });
 });
