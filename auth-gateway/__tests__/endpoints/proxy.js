de4e50d4c6cc3b2ac21b1a1300dde094def8b660
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-19 00:08:27 +0200

    Test headers in the proxied request

--- auth-gateway/__tests__/endpoints/proxy.js
+++ auth-gateway/__tests__/endpoints/proxy.js
@@ -3,20 +3,55 @@ const app = require("../../app");
 const supertest = require("supertest");
 const request = supertest(app);
 const nock = require("nock");
+const jwt = require("jsonwebtoken");
 
 describe("/*", () => {
-  it("should proxy the request", async () => {
-    // given
-    const path = "/custom-path";
+  describe("when no authorized Bearer token is in the header", () => {
+    it("should proxy the request without the x-user_id in the header", async () => {
+      // given
+      const path = "/custom-path";
 
-    nock(process.env.BACKEND_HOST).get(path).reply(200, {
-      messge: "OK",
+      nock(process.env.BACKEND_HOST)
+        .get(path)
+        .reply(200, {
+          reqheaders: {
+            "X-user_id": (headerValue) => !headerValue,
+          },
+        });
+
+      // when
+      const response = request.get(path);
+
+      // then
+      await response.expect(200);
     });
+  });
 
-    // when
-    const response = request.get(path);
+  describe("when authorized Bearer token is in the header", () => {
+    it("should proxy the request with the x-user_id included in the header", async () => {
+      // given
+      const sub = "user id";
 
-    // then
-    await response.expect(200);
+      const token = jwt.sign({ sub }, process.env.JWT_SECRET);
+
+      const path = "/custom-path";
+
+      nock(process.env.BACKEND_HOST)
+        .get(path)
+        .reply(200, {
+          reqheaders: {
+            "X-user_id": (headerValue) => headerValue === sub,
+          },
+        });
+
+      // when
+      const response = request
+        .get(path)
+        .set("Accept", "application/json")
+        .set("authorization", `Bearer ${token}`);
+
+      // then
+      await response.expect(200);
+    });
   });
 });
