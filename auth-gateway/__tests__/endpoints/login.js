0e08b17ea1276b01555c9fdee298053778ae1eb9
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-18 10:33:24 +0200

    Add test for login

---
+++ auth-gateway/__tests__/endpoints/login.js
@@ -0,0 +1,14 @@
+require("dotenv").config({ path: ".env.test" });
+const app = require("../../app");
+const supertest = require("supertest");
+const request = supertest(app);
+
+describe("/login", () => {
+  it("should redirect", async () => {
+    // when
+    const response = request.get(`/login`);
+
+    // then
+    await response.expect(302);
+  });
+});
