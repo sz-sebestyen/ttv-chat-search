81f5543e0ecd5ebab318c5df4423265917dcb1ff
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-19 22:45:34 +0200

    Add tests for POST /vod/:id/chat-search

---
+++ backend/__tests__/endpoints/vod_id_chat-search.spec.js
@@ -0,0 +1,59 @@
+require("dotenv").config({ path: ".env.test" });
+const app = require("../../app");
+
+const {
+  dbConnect,
+  dbDisconnect,
+  dbClearCollections,
+} = require("../__utils__/memoryMongoDB");
+const chatMessageSample = require("../__utils__/chatMessageSample");
+const { ChatMessage } = require("../../models");
+
+const supertest = require("supertest");
+const request = supertest(app);
+
+jest.mock("node-fetch", () => jest.fn());
+
+describe("POST /vod/:id/chat-search?term=<search-term>", () => {
+  describe("when term is missing", () => {
+    it("should return 400", async () => {
+      // when
+      const response = request
+        .post("/vod/123/chat-search")
+        .set("Accept", "application/json");
+
+      // then
+      await response.expect(400);
+    });
+  });
+
+  describe("when the search term is given", () => {
+    let mongoServer;
+
+    beforeEach(async () => {
+      mongoServer = await dbConnect();
+    });
+
+    afterEach(async () => {
+      await dbDisconnect(mongoServer);
+    });
+
+    it("should return the messages found", async () => {
+      // given
+      const vodId = "123";
+      const searchTerm = "find me";
+
+      chatMessageSample.content_id = vodId;
+      chatMessageSample.message.body = `Filler text 101 ${searchTerm} more filler text`;
+      await new ChatMessage(chatMessageSample).save();
+
+      // when
+      const response = await request
+        .post(`/vod/${vodId}/chat-search?term=${searchTerm}`)
+        .set("Accept", "application/json");
+
+      // then
+      expect(response.body).toHaveLength(1);
+    });
+  });
+});
