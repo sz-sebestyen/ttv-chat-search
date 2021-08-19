24a9a2135360d59b9f3357e5b85f56ef3bcb7089
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-19 14:20:49 +0200

    Protect search route from malformed requests

--- backend/controllers/searchInChat.js
+++ backend/controllers/searchInChat.js
@@ -4,6 +4,9 @@ const searchInChat = async (req, res, next) => {
   const { id } = req.params;
   const { term } = req.query;
 
+  if (!id) return res.status(400).json({ message: "Missing VOD id" });
+  if (!term) return res.status(400).json({ message: "Missing search term" });
+
   const messages = await searchMessages(id, term);
 
   res.json(messages);
