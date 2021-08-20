f0556b18ccd8c9ba6fe1738bc8d45ee58629a6d6
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-20 08:57:23 +0200

    Delete some comments

--- chat-downloader/app.js
+++ chat-downloader/app.js
@@ -2,17 +2,13 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 
-// controllers
 const { vodInfoResponse } = require("./controllers");
 
-// middlewares
 const { downloadChat, getVodInfo, errorHandler } = require("./middlewares");
 
-// app-wide middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes
 app.post("/vod/:id/chat", [getVodInfo, vodInfoResponse, downloadChat]);
 app.get("/vod/:id", [getVodInfo, vodInfoResponse]);
 
