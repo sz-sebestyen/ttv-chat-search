767ea220ed001ae502f76642692910b922e78cf3
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-22 01:07:10 +0200

    Style search history

--- frontend/src/pages/SearchHistory.jsx
+++ frontend/src/pages/SearchHistory.jsx
@@ -27,20 +27,28 @@ function SearchHistory() {
         <HiArrowLeft />
       </button>
 
-      {searches.map((search, index) => (
-        <div className="px-4 py-2 bg-surface my-1 flex" key={index}>
-          <a href={`/vod/${search.vodId}`} className="underline">
-            {search.vodId}
-          </a>
-          <span className="truncate">: {search.term}</span>
-          <a
-            href={`/vod/${search.vodId}/chat-search-result/${search.term}`}
-            className="ml-auto"
-          >
-            View
-          </a>
-        </div>
-      ))}
+      {searches
+        .map((search, index) => (
+          <div className="px-4 py-2 bg-surface my-1 flex text-sm" key={index}>
+            <span className="mr-2 text-gray-500">
+              {new Date(search.created_at).toLocaleDateString("en-GB")}
+            </span>
+            <a
+              href={`/vod/${search.vodId}`}
+              className="underline hover:text-violet-400"
+            >
+              {search.vodId}
+            </a>
+            <span className="truncate mr-2">: {search.term}</span>
+            <a
+              href={`/vod/${search.vodId}/chat-search-result/${search.term}`}
+              className="ml-auto hover:text-violet-400"
+            >
+              View
+            </a>
+          </div>
+        ))
+        .reverse()}
     </div>
   );
 }
