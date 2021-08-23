c9503f3a0853772043d5e79fa951ebd9d0800f5f
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-23 08:43:31 +0200

    Speed up chat download on longer VODs

--- chat-downloader/services/TwitchService/TwitchService.js
+++ chat-downloader/services/TwitchService/TwitchService.js
@@ -1,6 +1,17 @@
 const getVodInfo = require("./getVodInfo");
 const Chat = require("./Chat");
 const { ChatMessage, VodInfo } = require("../../models");
+const getSecondsFromDuration = require("./getSecondsFromDuration");
+
+const numberOfDefaultThreads = 4;
+const numberOfSecondsInAnHour = 3600;
+
+const getNumberOfDownloadThreads = (vodLengthInSeconds) => {
+  if (vodLengthInSeconds < numberOfSecondsInAnHour * 9)
+    return numberOfDefaultThreads;
+
+  return Math.floor(vodLengthInSeconds / numberOfSecondsInAnHour / 2);
+};
 
 const downloadChat = async (vodInfo) => {
   const hasDownloadStartedElsewhere = await ChatMessage.findOne({
@@ -13,7 +24,9 @@ const downloadChat = async (vodInfo) => {
     );
   }
 
-  const chat = new Chat(vodInfo, 4);
+  const vodLength = getSecondsFromDuration(vodInfo.duration);
+
+  const chat = new Chat(vodInfo, getNumberOfDownloadThreads(vodLength));
 
   const intervalId = setInterval(async () => {
     const downloadProgress = chat.getProgress();
