1b07e650424192ac12e57237493e7f9de7267647
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-24 16:37:08 +0200

    Add gif to not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 function NotFound() {
   return (
-    <div className="text-3xl text-center bg-background p-2">
-      404 Page not found
+    <div className="bg-background">
+      <div className="text-3xl text-center p-2">404 Page not found</div>
+
+      <div className="flex justify-center p-2">
+        <img
+          src="https://emoji.gg/assets/emoji/1928-modcheck.gif"
+          width="64px"
+          height="64px"
+          alt="modCheck"
+        />
+      </div>
     </div>
   );
 }
